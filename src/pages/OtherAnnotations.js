c55f29c469f57831529cafc6cc8a7c49ea968fcd
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2024-10-26 15:21:48 +0800

    integrate annotations step radio buttons from types

--- src/pages/OtherAnnotations.js
+++ src/pages/OtherAnnotations.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { AnnotationStepPage } from '../components';
+import { AnnotationStepPage, HeadingStep } from '../components';
 import Context from '../context';
+
+import annotationTypesAll from '../data/other-annotation-types';
+import { utils } from '../constants';
 
 function OtherAnnotations() {
   const cnxt = React.useContext(Context);
@@ -8,6 +11,8 @@ function OtherAnnotations() {
 
   // ui state
   const routeName = 'Other annotations';
+  const annotationTypes = annotationTypesAll;
+  const annotationTypesArray = Object.keys(annotationTypes);
 
   return (
     <AnnotationStepPage
@@ -16,8 +21,38 @@ function OtherAnnotations() {
       bannerTipProps={{ pageType, routeName }}
     >
       <React.Fragment>
-        <div>Dummy component</div>
-        <div>Bootstrapping</div>
+        <HeadingStep
+          number={1}
+          text="Hold Ctrl/Cmd to select layer in your mock that needs annotating, and choose type of annotation."
+        />
+
+        <div className="button-group" role="radiogroup">
+          {annotationTypesArray.map((type) => {
+            const { label, icon } = annotationTypes[type];
+
+            const onClick = () => {
+              // onInitialTypeSelect(type); TODO
+            };
+
+            return (
+              <div key={label} className="container-selection-button">
+                <div
+                  className="selection-button"
+                  onClick={onClick}
+                  onKeyDown={(e) => {
+                    if (utils.isEnterKey(e.key)) onClick();
+                  }}
+                  role="button"
+                  tabIndex={0}
+                >
+                  <div>{icon}</div>
+                </div>
+
+                <div className="selection-button-label">{label}</div>
+              </div>
+            );
+          })}
+        </div>
       </React.Fragment>
     </AnnotationStepPage>
   );
